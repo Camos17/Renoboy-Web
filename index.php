7cb3b07e69e44a17e672d656c00ad0d0dd870f27
Familiar2 <Familiar2>
2016-02-26 13:03:19 -0500

    Ajustes Header y Footer en todas las resoluciones de pantalla

--- index.php
+++ index.php
@@ -52,22 +52,22 @@
 			</div><!-- /.container-fluid -->
 		</nav>		
 		<div class="col-xs-12 no-padding content-wrapper">
-			<video class="hidden-xs hidden-sm hidden-md img-responsive"  preload="auto" poster="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-2cd50778d61e6b93b83bfc963a2ecb07.jpg" autoplay loop>
+			<video class="hidden-xs hidden-sm img-responsive"  preload="auto" poster="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-2cd50778d61e6b93b83bfc963a2ecb07.jpg" autoplay loop>
 				<source src="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-9ec94a53ca64078e60d0f2a09491b35a.mp4" type="video/mp4">
 				<source src="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-9ec94a53ca64078e60d0f2a09491b35a.webm" type="video/webm">
 				Your browser does not support the video tag.
 			</video> 
-			<img class="hidden-lg img-responsive " src="http://placehold.it/320x460" alt="...">
+			<img class="hidden-md hidden-lg img-responsive " src="http://placehold.it/320x460" alt="...">
 		</div>
 
 		<footer class="col-xs-12 no-padding estilo-footer">
-			<div class="col-xs-12 col-md-3 col-md-push-3 no-padding texto-sedes">
+			<div class="col-xs-12 hidden-md hidden-lg no-padding texto-sedes">
 				<p class="text-center"><strong>Planta Renoboy Bogotá</strong><br>
 				   Calle 17A N° 69F - 58 Zona Industrial Montevideo<br>
 				   <a href="tel: Tel: (+571) 742 2779 ">Tel: (+571) 742 2779</a></p>
 			</div>
 
-			<div id="carousel-example-generic" class="carousel slide carousel-legal col-xs-12 col-md-3 col-md-pull-3" data-ride="carousel" data-interval="false">
+			<div id="carousel-example-generic" class="carousel slide carousel-legal col-xs-12 col-md-3" data-ride="carousel" data-interval="false">
 			 
 			  <!-- Wrapper for slides -->
 				<div class="carousel-inner">
@@ -106,18 +106,18 @@
 				</a>
 			</div> <!-- Carousel -->
 
-			<div class="col-xs-12 col-md-6 no-padding">
+			<div class="col-xs-12 col-md-9 col-lg-8 c2-footer">
 				
 			
 				<div class="col-xs-12 col-md-4 no-padding texto-sedes2">
-					<p class="text-center"><strong>Planta Renoboy Girón</strong><br>
-					Carrera 13 N° 70 - 154 Km 6 Autopista Girón<br>
-					<a href="tel: (+577) 697 2696">Tel: (+577) 697 2696</a><br><br>
+					<p class="text-center"><strong>Planta Renoboy Bogotá</strong><br>
+					Calle 17A N° 69F - 58 Zona Industrial Montevideo<br>
+				  	<a href="tel: Tel: (+571) 742 2779 ">Tel: (+571) 742 2779</a><br><br>
 					<strong>Planta Renoboy Ibagué</strong><br>
 					Carrera 48 Sur N° 92 - 15 Vía Picaleña<br>
 					<a href="tel: (+578) 277 1099">Tel: (+578) 277 1099</a></p>
 				</div>
-				<div class="col-xs-12 no-padding col-md-8 visible-md texto-sedes2">
+				<div class="col-xs-12 no-padding col-md-8 visible-md visible-lg texto-sedes2">
 					<div class="col-md-6">
 						<p class="text-center"><strong>Planta Renoboy Yumbo</strong><br>
 						Carrera 20G N° 14B - 36 Cencar Yumbo<br>
@@ -129,7 +129,11 @@
 					<div class="col-md-6 texto-sedes2 texto-sedes3">
 						<p class="text-center"><strong>Planta Renoboy Tubará</strong><br>
 						Vía Tubará Kilómetro 11 Diagonal Relleno AAA<br>
-						<a href="tel: (+575) 385 6034">Tel: (+575) 385 6034</a><br><br></p>
+						<a href="tel: (+575) 385 6034">Tel: (+575) 385 6034</a><br><br>
+						<strong>Planta Renoboy Girón</strong><br>
+						Carrera 13 N° 70 - 154 Km 6 Autopista Girón<br>
+						<a href="tel: (+577) 697 2696">Tel: (+577) 697 2696</a><br><br>
+						</p>
 					</div>					
 				</div>
 				<div id="mas-sedes" class="col-xs-12 hidden-md hidden-lg no-padding texto-sedes2">
