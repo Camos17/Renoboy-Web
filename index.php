9980bc475b1ddb6204f2ecc907f8a881625c1eab
Familiar2 <Familiar2>
2016-03-01 15:29:34 -0500

    Se agregan barra de navegacion para los icono siempre visibles.

--- index.php
+++ index.php
@@ -31,10 +31,10 @@
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
-							<li class="col-md-4 col-lg-2 no-padding menu-list1"><a href="#">Noticias de Interes<span class="sr-only">(current)</span></a></li>
-							<li class="col-md-4 col-lg-3 no-padding menu-list1"><a href="#"><img  class="hidden-xs icon-header" src="img/iconoheader1.svg" alt="">Acceso a Distribuidores</a></li>
-							<li class="col-md-4 col-lg-3 no-padding menu-list1"><a href="#"><img class="hidden-xs icon-header" src="img/iconoheader2.svg" alt="">Pensando en Reencauchar</a></li>
-							<li class="col-lg-4 no-padding hidden-xs hidden-sm hidden-md menu-list1">
+							<li class="col-md-4 col-lg-2 no-padding"><a href="#">Noticias de Interes<span class="sr-only">(current)</span></a></li>
+							<li class="col-md-4 col-lg-3 no-padding"><a href="#"><img  class="hidden-xs icon-header" src="img/iconoheader1.svg" alt="">Acceso a Distribuidores</a></li>
+							<li class="col-md-4 col-lg-3 no-padding"><a href="#"><img class="hidden-xs icon-header" src="img/iconoheader2.svg" alt="">Pensando en Reencauchar</a></li>
+							<li class="col-lg-4 no-padding hidden-xs hidden-sm hidden-md">
 								<form class="col-md-12 no-padding" role="search">
 									<button type="submit" class="col-lg-2 btn btn-default btn-header-busqueda"><img src="img/iconoheader-buscar.svg" alt=""></button>
 									<div class="col-lg-10 searchinput">
@@ -46,28 +46,53 @@
 							<li class="col-sm-2 no-padding"><a href="#">Productos</a></li>
 							<li class="col-sm-3 no-padding"><a href="#">Red de Distribucion</a></li>
 							<li class="col-sm-5 col-md-4 no-padding"><a href="#"><img class="hidden-xs icon-header" src="img/iconoheader3.svg" alt="">Kilometros Generando Valor</a></li>
-							<li class="col-sm-2 no-padding"><a href="#">Contacto</a></li>							
+							<li class="col-sm-2 no-padding"><a href="#">Contacto</a></li>	
 						</ul>
 				</div><!-- /.navbar-collapse -->
 			</div><!-- /.container-fluid -->
 		</nav>	
-		<div class="col-xs-12 no-padding content-wrapper">
-			<video class="hidden-xs hidden-sm img-responsive"  preload="auto" poster="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-2cd50778d61e6b93b83bfc963a2ecb07.jpg" autoplay loop>
+		<div class="hidden-xs hidden-sm col-xs-12 no-padding content-wrapper" data-vide-bg="https://s3-us-west-2.amazonaws.com/cannedhead.optica/optica" data-vide-options="loop: true, muted: false, position: 0% 0%">
+			<!-- <video class=""  preload="auto" poster="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-2cd50778d61e6b93b83bfc963a2ecb07.jpg" autoplay loop>
 				<source src="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-9ec94a53ca64078e60d0f2a09491b35a.mp4" type="video/mp4">
 				<source src="http://d2bptzq05weps4.cloudfront.net/assets/video_headers/homepage/home_video_03_2015_lumia-9ec94a53ca64078e60d0f2a09491b35a.webm" type="video/webm">
 				Your browser does not support the video tag.
-			</video> 
-			<img class="hidden-md hidden-lg img-responsive " src="http://placehold.it/320x460" alt="...">
+			</video>  -->
 		</div>
-
+		<div class="hidden-md hidden-lg content-wrapper">
+			<img class="hidden-md hidden-lg img-responsive" src="http://placehold.it/320x460" alt="...">
+		</div>
+		<nav class="col-xs-12 nav-iconos-fijos">
+			<div id="carousel-example-generic" class="carousel slide carousel-legal col-xs-12 col-md-3 col-md-pull-3 col-lg-3 col-lg-pull-3" data-ride="carousel" data-interval="false">
+				<ul class="col-xs-12 no-padding">
+					<li class="col-xs-2">
+						<a href="#" title=""><img class="icono-fijo" src="img/botonmenuderecha1.svg" alt=""></a>
+					</li>
+					<li class="col-xs-2">
+						<a href="#" title=""><img class="icono-fijo" src="img/botonmenuderecha2.svg" alt=""></a>
+					</li>
+					<li class="col-xs-2">
+						<a href="#" title=""><img class="icono-fijo" src="img/botonmenuderecha3.svg" alt=""></a>
+					</li>
+					<li class="col-xs-2">
+						<a href="#" title=""><img class="icono-fijo" src="img/botonmenuderecha4.svg" alt=""></a>
+					</li>
+					<li class="col-xs-2">
+						<a href="#" title=""><img class="icono-fijo" src="img/botonmenuderecha5.svg" alt=""></a>
+					</li>
+					<li class="col-xs-2">
+						<a href="#" title=""><img class="icono-fijo" src="img/botonmenuderecha6.svg" alt=""></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 		<footer class="col-xs-12 no-padding estilo-footer">
-			<div class="col-xs-12 hidden-md hidden-lg no-padding texto-sedes">
+			<div class="col-xs-12 col-md-3 col-md-push-3 col-lg-3 col-lg-push-3 no-padding texto-sedes">
 				<p class="text-center"><strong>Planta Renoboy Bogotá</strong><br>
 				   Calle 17A N° 69F - 58 Zona Industrial Montevideo<br>
 				   <a href="tel: Tel: (+571) 742 2779 ">Tel: (+571) 742 2779</a></p>
 			</div>
 
-			<div id="carousel-example-generic" class="carousel slide carousel-legal col-xs-12 col-md-3" data-ride="carousel" data-interval="false">
+			<div id="carousel-example-generic" class="carousel slide carousel-legal col-xs-12 col-md-3 col-md-pull-3 col-lg-3 col-lg-pull-3" data-ride="carousel" data-interval="false">
 			 
 			  <!-- Wrapper for slides -->
 				<div class="carousel-inner">
@@ -106,7 +131,7 @@
 				</a>
 			</div> <!-- Carousel -->
 
-			<div class="col-xs-12 col-md-9 col-lg-8 c2-footer">
+			<div class="col-xs-12 col-md-6 col-lg-6 c2-footer">
 				
 			
 				<div class="col-xs-12 col-md-4 no-padding texto-sedes2">
@@ -120,7 +145,8 @@
 				<div class="col-xs-12 no-padding col-md-8 visible-md visible-lg texto-sedes2">
 					<div class="col-md-6">
 						<p class="text-center"><strong>Planta Renoboy Yumbo</strong><br>
-						Carrera 20G N° 14B - 36 Cencar Yumbo<br>
+						Carrera 20G N° 14B - 36 
+						 Yumbo<br>
 						<a href="tel: (+572) 485 3453">Tel: (+572) 485 3453</a><br><br>
 						<strong>Planta Renoboy Sabaleta</strong><br>
 						Carrera 47 E N° 78C Sur - 95 Ciudadela Industrial ACIC<br>
@@ -160,6 +186,7 @@
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.vide.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
 				$( "#btn-mas-sedes" ).click(function() {
