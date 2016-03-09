bb97d344daa7f29afc19ca7e8f156e0bee29686f
camos17 <camos8710@gmail.com>
2016-03-09 02:06:18 -0500

    Mas ajustes vista de noticias en todas las resoluciones

--- noticias.php
+++ noticias.php
@@ -2,42 +2,65 @@
 
 		
 		<div class="col-xs-12 no-padding content-wrapper noticias">
-			<div class="col-xs-12 col-sm-4 no-padding">
-				<div class="col-xs-12">
+			<div class="col-sm-5 col-sm-offset-1 col-md-4 col-md-offset-0 no-padding c1-noticias">
+				<div class="col-xs-12 c1-noticias-headline">
 					<h1 class="text-center">NOTICIAS DE INTERÉS</h1>	
-				</div>				
-				<a class="col-xs-12" href="noticiasdesplegadas.php" title="">
-					<h2>Tu llanta nueva de nuevo</h2>
-					<div class="col-xs-12">
-						<hr>	
-					</div>					
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><i class="fa fa-clock-o">Actualizado hace 3 horas</i>			
+				</div>
+				<div class="col-xs-12 no-padding noticias-wrapper">				
+					<div class="col-xs-12 noticia">
+						<h2>Tu llanta nueva de nuevo</h2>
+						<div class="col-xs-12">
+							<hr>	
+						</div>					
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><span><i class="fa fa-clock-o"></i>Actualizado hace 3 horas</span>
+						<a href="noticiasdesplegadas.php" title="">Leer más</a>	
+						</p>
+					</div>
+					<div class="col-xs-12 noticia" >
+						<h2>Tu llanta nueva de nuevo</h2>
+						<div class="col-xs-12">
+							<hr>	
+						</div>					
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><span><i class="fa fa-clock-o"></i>Actualizado hace 3 horas</span>
+						<a href="noticiasdesplegadas.php" title="">Leer más</a>	
+						</p>
+					</div>
+					<div class="col-xs-12 noticia">
+						<h2>Tu llanta nueva de nuevo</h2>
+						<div class="col-xs-12">
+							<hr>	
+						</div>					
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><span><i class="fa fa-clock-o"></i>Actualizado hace 3 horas</span>
+						<a href="noticiasdesplegadas.php" title="">Leer más</a>	
+						</p>
+					</div>
+					<div class="col-xs-12 noticia">
+						<h2>Tu llanta nueva de nuevo</h2>
+						<div class="col-xs-12">
+							<hr>	
+						</div>					
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><span><i class="fa fa-clock-o"></i>Actualizado hace 3 horas</span>
+						<a href="noticiasdesplegadas.php" title="">Leer más</a>	
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="hidden-xs col-sm-6 col-md-4 c2-noticias">
+				<img class="img-responsive" src="http://placehold.it/225x350" alt="">	
+			</div>
+			<div class="col-xs-12 col-sm-4 hidden-xs hidden-sm noticia-desplegada">
+				<h2>Tu llanta nueva de nuevo</h2>
+				<div class="col-xs-12 col-sm-12 separador">
+					<hr>	
+				</div>
+				<div class="col-xs-12 col-sm-12 no-padding wrapper-noticia-desplegada">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><br>
 					</p>
-				</a>
-				<a class="col-xs-12" href="#" title="">
-					<h2>Tu llanta nueva de nuevo</h2>
-					<div class="col-xs-12">
-						<hr>	
-					</div>					
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><i class="fa fa-clock-o">Actualizado hace 3 horas</i>			
-					</p>
-				</a>
-				<a class="col-xs-12" href="#" title="">
-					<h2>Tu llanta nueva de nuevo</h2>
-					<div class="col-xs-12">
-						<hr>	
-					</div>					
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><i class="fa fa-clock-o">Actualizado hace 3 horas</i>			
-					</p>
-				</a>
-				<a class="col-xs-12" href="#" title="">
-					<h2>Tu llanta nueva de nuevo</h2>
-					<div class="col-xs-12">
-						<hr>	
-					</div>					
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><i class="fa fa-clock-o">Actualizado hace 3 horas</i>			
-					</p>
-				</a>
+				</div>			
 			</div>
 		</div>
 		
--- noticiasdesplegadas.php
+++ noticiasdesplegadas.php
@@ -2,17 +2,24 @@
 
 		
 		<div class="col-xs-12 no-padding content-wrapper noticias">
-			<div class="col-xs-12 noticia-desplegada">
+			<div class="hidden-xs col-sm-5 col-sm-offset-1 col-md-4 img-noticia-desplegada">
+				<img class="img-responsive" src="http://placehold.it/225x385" alt="">	
+			</div>
+			<div class="col-xs-12 col-sm-5 noticia-desplegada">
 				<h2>Tu llanta nueva de nuevo</h2>
-				<div class="col-xs-12">
+				<div class="col-xs-12 col-sm-12 separador">
 					<hr>	
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
-				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
-				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><br>
-				</p>
-				<div class="col-xs-12 no-padding btn-regreso-noticia">
-					<a class="col-xs-9 col-xs-offset-3" href="noticias.php" title=""><i class="fa fa-chevron-circle-left"></i>Noticias de Interés</a>
+				<div class="col-xs-12 col-sm-12 no-padding wrapper-noticia-desplegada">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><br>
+					</p>
+				</div>
+				<div class="col-xs-12 col-sm-12 no-padding btn-regreso-noticia">
+					<a href="noticias.php"><i class="fa fa-chevron-circle-left"></i>Noticias de Interés</a>
 				</div>			
 			</div>
 		</div>
