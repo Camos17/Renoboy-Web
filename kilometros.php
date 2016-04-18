aff22bbf352fb40c7247dc71c21357c0f6926db4
camos17 <camos8710@gmail.com>
2016-04-18 00:52:02 -0500

    mas ajustes para la vista kilometros generando valores

--- kilometros.php
+++ kilometros.php
@@ -3,31 +3,42 @@
 	<div class="col-xs-12 wrapper-contacto no-padding fondo-kilometros-generando-valor">
 		<div class="col-xs-12 layer-fondo-kilometros-generando-valor">
 			<div class="col-xs-12 contenido-kilometros-generando-valor no-padding">
-				<a class="col-xs-12 col-sm-4 col-lg-4 no-padding fondo-kilometros1" href="#" title="">
-				<div class="col-xs-12 no-padding layer-kilometros">
+				<a class="col-xs-12 col-sm-3 col-lg-3 no-padding fondo-kilometros1" href="#" title="">
+					<div class="col-xs-12 no-padding layer-kilometros">
 						<p class="col-xs-12 text-center">28 AÑOS <br> GENERANDO VALOR</p>
 					</div>
 				</a>
-				<div class="col-xs-12 col-sm-4 col-lg-4 no-padding fondo-kilometros2">
+				<div class="col-xs-12 col-sm-3 col-lg-3 no-padding fondo-kilometros2">
 					<div class="col-xs-12 no-padding fondo-mision">
-						<div class="col-xs-12 no-padding layer-kilometros-mision">
-							<a class="col-xs-12 text-center no-padding" href="#" title="">MISIÓN</a>
-							<a class="col-xs-12 text-center no-padding" href="#" title="">VISIÓN</a>
-							<a class="col-xs-12 text-center no-padding" href="#" title="">VALORES</a>
-						</div>
+						<a class="col-xs-12 text-center no-padding" href="#" title="">
+							<div class="col-xs-12 no-padding layer-kilometros-mision">
+								<p class="col-xs-12 text-center">MISIÓN</p>
+							</div>
+						</a>
+						<a class="col-xs-12 text-center no-padding" href="#" title="">
+							<div class="col-xs-12 no-padding layer-kilometros-mision">
+								<p class="col-xs-12 text-center">VISIÓN</p>
+							</div>
+						</a>
+						<a class="col-xs-12 text-center no-padding" href="#" title="">
+							<div class="col-xs-12 no-padding layer-kilometros-mision">
+								<p class="col-xs-12 text-center">VALORES</p>
+							</div>
+						</a>
 					</div>
-					<div class="col-xs-12 no-padding fondo-certificaciones">
-						<div class="col-xs-12 layer-kilometros-certificaciones">
-							
+					<a class="col-xs-12 no-padding fondo-certificaciones" href="#" title="">
+						<div class="col-xs-12 no-padding layer-kilometros-certificaciones">
+							<p class="col-xs-12 text-center">CERTIFICACIONES DE <br> CALIDAD RENOBOY</p>
 						</div>
-					</div>
+					</a>
 				</div>
-				<div class="col-xs-12 col-sm-4 col-lg-4 no-padding fondo-kilometros3">
+				<a class="col-xs-12 col-sm-3 col-lg-3 no-padding fondo-kilometros3">
 					<div class="col-xs-12 no-padding layer-kilometros">
-						
+						<p class="col-xs-12 text-center">VISITA VIRTUAL <br> A LA PLANTA</p>
+						<a class="col-xs-6 col-xs-offset-3 btn-visita-virtual" href="#" title="">ver más</a>
 					</div>
 				</div>
-				<div class="col-xs-12 col-sm-4 col-lg-4 no-padding fondo-kilometros4">
+				<div class="col-xs-12 col-sm-3 col-lg-3 no-padding fondo-kilometros4">
 					<div class="col-xs-12 no-padding layer-kilometros">
 						
 					</div>
