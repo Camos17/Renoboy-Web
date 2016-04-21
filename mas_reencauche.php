da076903f50cf47b08d3ccb519c8a5edce2ef7a4
camos17 <camos8710@gmail.com>
2016-04-21 01:00:27 -0500

    mas ajustes vista interna mas sobre nuestro reencauche

--- mas_reencauche.php
+++ mas_reencauche.php
@@ -1,46 +1,49 @@
 	<?php include "header.php";?>
 
-	<div class="col-xs-12 no-padding fondo-mas-reencauche">
-		<div class="col-xs-12 layer-fondo-mas-reencauche">
-			<div class="col-xs-12 col-sm-10 col-sm-offset-1 contenido-mas-reencauche no-padding">
-				<div class="col-xs-12 col-md-4 no-padding contenido-mas-reencauche1">
-					<div class="col-xs-12 no-padding headline-mas-reencauche1">
-						<h2>MÁS SOBRE NUESTRO REEENCAUCHE</h2>
+		<div class="col-xs-12 no-padding fondo-mas-reencauche">
+			<div class="col-xs-12 layer-fondo-mas-reencauche">
+				<div class="col-xs-12 col-sm-10 col-sm-offset-1 contenido-mas-reencauche no-padding">
+					<div class="col-xs-12 col-md-4 no-padding contenido-mas-reencauche1">
+						<div class="col-xs-12 no-padding headline-mas-reencauche1">
+							<h2>MÁS SOBRE NUESTRO REEENCAUCHE</h2>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. </p>
 					</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. </p>
-				</div>
-				<div class="col-xs-12 col-md-8 contenido-mas-reencauche2">
-					<img class="img-responsive" src="img/massobrenuestroreencauche.svg" alt="">
+					<div class="col-xs-12 col-md-8 contenido-mas-reencauche2">
+						<img class="img-responsive" src="img/massobrenuestroreencauche.svg" alt="">
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="col-xs-12 no-padding fondo-mas-reencauche2">
-		<div class="col-xs-12 text-center no-padding headline-mas-reencauche2">
-			<h2>MÁS SOBRE NUESTRO REEENCAUCHE</h2>
-			<div class="col-xs-12 no-padding">
-				<hr>
+		<div class="col-xs-12 no-padding fondo-mas-reencauche2">
+			<div class="col-xs-12 text-center no-padding headline-mas-reencauche2">
+				<h2>MÁS SOBRE NUESTRO REEENCAUCHE</h2>
+				<div class="col-xs-12 no-padding">
+					<hr>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-10 col-sm-offset-1 img-proceso-reencauche">
+				<img class="img-responsive" src="img/serviciodereencauche.svg" alt="">
+			</div>
+			<div class="col-xs-12 no-padding fondo-camino-reencauche">
+				<div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 camino-reencauche">
+					<img  class="img-responsive" src="img/carretera.svg" alt="">
+				</div>
+				<div class="col-xs-12 no-padding frame-carretera">
+					
+				</div>
+				<div class="no-padding ubicacion-camion-carretera">
+					<img class="img-responsive" src="img/camion.svg" alt="">
+				</div>
 			</div>
 		</div>
-		<div class="col-xs-12 col-sm-10 col-sm-offset-1 img-proceso-reencauche">
-			<img class="img-responsive" src="img/serviciodereencauche.svg" alt="">
-		</div>
-		<div class="col-xs-12 no-padding fondo-camino-reencauche">
-			<div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 camino-reencauche">
-				<img  class="img-responsive" src="img/carretera.svg" alt="">
-			</div>
-			<div class="col-xs-12 no-padding frame-carretera">
-				
-			</div>
-			<div class="no-padding ubicacion-camion-carretera">
-				<img class="img-responsive" src="img/camion.svg" alt="">
-			</div>
-		</div>
-	</div>
+
+	<?php include "menu-fijo.php";?>		
+	<?php include "footer.php";?>
