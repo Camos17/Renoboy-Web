da58fa522fe7177bcfc9eadfcff60ae3d9cdf392
camos17 <camos8710@gmail.com>
2016-04-22 10:16:08 -0500

    neic

--- mas_reencauche.php
+++ mas_reencauche.php
@@ -15,8 +15,11 @@
 						proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 						tempor incididunt ut labore et dolore magna aliqua. </p>
 					</div>
-					<div class="col-xs-12 col-md-8 contenido-mas-reencauche2">
-						<img class="img-responsive" src="img/massobrenuestroreencauche.svg" alt="">
+					<div class="col-xs-12 col-md-8 hidden-xs hidden-sm contenido-mas-reencauche2">
+						<img class="img-responsive" src="img/massobrenuestroreencauche.svg" alt="...">
+					</div>
+					<div class="col-xs-12 col-md-8 hidden-md hidden-lg contenido-mas-reencauche2">
+						<img class="img-responsive" src="img/massobrenuestroreencauche_mobile.svg" alt="...">
 					</div>
 				</div>
 			</div>
@@ -29,21 +32,68 @@
 					<hr>
 				</div>
 			</div>
-			<div class="col-xs-12 col-sm-10 col-sm-offset-1 img-proceso-reencauche">
+			<div class="col-xs-12 col-sm-10 col-sm-offset-1 hidden-xs hidden-sm img-proceso-reencauche">
 				<img class="img-responsive" src="img/serviciodereencauche.svg" alt="">
 			</div>
+			<div class="col-xs-6 col-xs-offset-3 col-sm-10 col-sm-offset-1 hidden-md hidden-lg img-proceso-reencauche">
+				<img class="img-responsive" src="img/serviciodereencauche_mobile.svg" alt="">
+			</div>
 			<div class="col-xs-12 no-padding fondo-camino-reencauche">
+				<img class="col-xs-12 no-padding img-fondo-camino-reencauche" src="img/fondoverde.svg" alt="...">
 				<div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 camino-reencauche">
 					<img  class="img-responsive" src="img/carretera.svg" alt="">
 				</div>
 				<div class="col-xs-12 no-padding frame-carretera">
 					
 				</div>
-				<div class="no-padding ubicacion-camion-carretera">
-					<img class="img-responsive" src="img/camion.svg" alt="">
+				<div class="col-xs-2 col-xs-offset-5 no-padding ubicacion-camion-carretera">
+					<div class="col-xs-6 col-xs-offset-3 no-padding">
+						<img class="img-responsive" src="img/camion.svg" alt="">
+					</div>
+				</div>
+				<div class="col-xs-8 col-xs-offset-2 btn-modal-reencauche">
+					<a class="btn btn-info btn-block" data-toggle="modal" data-target="#reencaucheModal">
+					Conoce más de <br> nuestro reencauche
+					</a>
 				</div>
 			</div>
 		</div>
+
+		<!-- Modal -->
+		<div class="modal fade" id="reencaucheModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		    	<div class="modal-header headline-ayuda-buscador">
+		        	<button type="button" class="close cerrar-ayuda" data-dismiss="modal" aria-label="Close">
+		        		<span aria-hidden="true">
+		        			<i class="ion-android-close"></i>
+		        		</span>
+		        	</button>
+		    	</div>
+		      <div class="modal-body">
+		      	<div class="row">      	
+			        <div class="col-xs-8">
+			        	<img class="img-responsive" src="img/esseguro.svg" alt="">
+			        </div>
+			        <div class="col-xs-12">
+			        	<p>Lorem Ipsum</p>
+		        	</div>
+		        	<hr class="col-xs-12">
+		        	<div class="col-xs-8">
+			        	<img class="img-responsive" src="img/esseguro.svg" alt="">
+			        </div>
+			        <div class="col-xs-12">
+			        	<p>Lorem Ipsum</p>
+		        	</div>
+		        </div>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary">Save changes</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>	
 
 	<?php include "menu-fijo.php";?>		
 	<?php include "footer.php";?>
